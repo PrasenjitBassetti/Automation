08c5826eb6f8fcc4861affff91f79b6f7ffb6435
Prasenjit  Chowdhury <prasenjit.chowdhury.bassetti@gmail.com>
2018-02-04 09:31:46 +0530

    Remove Unused package from TestBaseSetup

--- SeleniumFramework/src/main/java/testdrive/TestBaseSetup.java
+++ SeleniumFramework/src/main/java/testdrive/TestBaseSetup.java
@@ -12,7 +12,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
+
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -20,7 +20,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
